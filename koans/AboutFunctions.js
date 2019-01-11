4dbf13097cee4b4c6f2c77e76c22ac178d377838
Wendy Huynh <wendyhuynhs@gmail.com>
2019-01-11 15:50:55 -0800

    updated answers

--- koans/AboutFunctions.js
+++ koans/AboutFunctions.js
@@ -74,7 +74,7 @@ describe("About Functions", function() {
       return argsArray.join(",");
     }
     
-    expect(returnAllArgs("first", "second", "third")).toBe("first, second, third");
+    expect(returnAllArgs("first", "second", "third")).toBe("first,second,third");
   });
 
   it("should pass functions as values", function () {
@@ -103,6 +103,9 @@ describe("About Functions", function() {
       //An internal comment
       return a * b;
     };
-    expect(multiply.toString()).toBe("2");
+    expect(multiply.toString()).toBe(`function (a, b) {
+      //An internal comment
+      return a * b;
+    }`);
   });    
 });
